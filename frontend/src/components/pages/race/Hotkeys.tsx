13d00bf7e63931139a1292c7f17a05694622edab
yerlantemir <temirerlan99@gmail.com>
2023-05-30 11:33:01 +0600

    feat: listen operator, operatorMotion commands execution

--- frontend/src/components/pages/race/Hotkeys.tsx
+++ frontend/src/components/pages/race/Hotkeys.tsx
@@ -2,22 +2,66 @@
 
 import { useEffect, useState } from 'react';
 
+type ExecutedCommand = {
+  isFailed: boolean;
+  command: string;
+};
 export const Hotkeys = () => {
-  const [pressedKeys, setPressedKeys] = useState<
-    {
-      keys: string;
-      isFinished: boolean;
-    }[]
-  >([]);
+  const [executedCommands, setExecutedCommands] = useState<ExecutedCommand[]>(
+    []
+  );
+  const [currentCommand, setCurrentCommand] = useState('');
+  const [lastPressedKey, setLastPressedKey] = useState('');
+
   useEffect(() => {
-    const onOperatorFinish = (event: any) => {
+    const onVimCommand = (event: any) => {
+      console.log(event.detail);
+      if (event.detail.type === 'operator') {
+        // g~, gu, gU, gg, gw...
+        if (event.detail.keys.length > 1) {
+          setExecutedCommands([
+            ...executedCommands,
+            { isFailed: false, command: event.detail.keys }
+          ]);
+          return;
+        }
+        const pressedKey = event.detail.keys;
+        if (currentCommand === '') {
+          setCurrentCommand(pressedKey);
+          return;
+        }
+        setExecutedCommands([
+          ...executedCommands,
+          {
+            isFailed: pressedKey !== currentCommand,
+            command: pressedKey + currentCommand
+          }
+        ]);
+        setCurrentCommand('');
+      }
+      if (event.detail.type === 'operatorMotion') {
+        setExecutedCommands([
+          ...executedCommands,
+          { isFailed: false, command: event.detail.keys }
+        ]);
+      }
+      if (event.detail.type === 'motion') {
+        const pressedKeys = event.detail.keys as string;
+        // handle diw, viw, ciw, yiw, di", ci", yi", vi", di}, ci}, yi}, vi}
+      }
+    };
+    const onVimKey = (event: any) => {
+      setLastPressedKey(event.detail);
       console.log(event.detail);
     };
-    const onOperatorStart = (event: any) => {
-      console.log(event.detail);
+
+    window.addEventListener('vimracing-key', onVimKey);
+    window.addEventListener('vimracing-command', onVimCommand);
+    return () => {
+      window.removeEventListener('vimracing-command', onVimCommand);
+
+      window.removeEventListener('vimracing-key', onVimKey);
     };
-    window.addEventListener('vimracing-operator-finish', onOperatorFinish);
-    window.addEventListener('vimracing-operator-start', onOperatorStart);
-  }, []);
-  return <div>hello world!</div>;
+  }, [currentCommand, executedCommands]);
+  return <div>{JSON.stringify(executedCommands)}</div>;
 };
