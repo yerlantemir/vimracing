cb26c8fed5f27e3f8b7b0f10ea743421e6b41479
yerlantemir <temirerlan99@gmail.com>
2023-07-17 12:01:40 +0600

    get training race endpoint improvements

--- backend/src/training/getTrainingRace.ts
+++ backend/src/training/getTrainingRace.ts
@@ -1,29 +1,39 @@
 import { Router } from 'express';
 import path from 'path';
 import fs from 'fs';
+import { SupportedLanguages } from '@vimracing/shared';
+import util from 'util';
 
 const racesDataPath = path.resolve(__dirname, '../../racesData');
 const router = Router();
 
-router.get('/', (req, res) => {
-  const lang = 'js';
+const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+router.get('/', async (req, res) => {
+  const params = req.query;
+  const { raceLangs } = params;
 
-  const raceMaxIndex = fs.readdirSync(
-    path.resolve(__dirname, `${racesDataPath}/${lang}`)
-  ).length;
-  const randomRaceId = Math.floor(Math.random() * raceMaxIndex);
+  const result = {} as Record<SupportedLanguages, any>;
 
-  fs.readFile(
-    `${racesDataPath}/${lang}/${randomRaceId}.json`,
-    'utf8',
-    (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      res.send(data);
+  try {
+    for (const lang of raceLangs as SupportedLanguages[]) {
+      const files = await readdir(
+        path.resolve(__dirname, `${racesDataPath}/${lang}`)
+      );
+      const raceMaxIndex = files.length;
+      const randomRaceId = Math.floor(Math.random() * raceMaxIndex);
+
+      const data = await readFile(
+        `${racesDataPath}/${lang}/${randomRaceId}.json`,
+        'utf8'
+      );
+      result[lang as SupportedLanguages] = JSON.parse(data);
     }
-  );
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal error');
+  }
 });
 
 export default router;
